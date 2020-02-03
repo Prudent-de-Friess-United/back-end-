890d74ebf1674e2dd78514a6ecde2c1c72cb0bc1
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-02-03 13:17:04 -0500

    specified routes and attemped tests - commented out routes.spec.js.
    Co-authored-by: ikeman32<ikeman32@yahoo.com>

---
+++ config/routes.js
@@ -0,0 +1,11 @@
+// IMPORTS
+
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const itemsRouter = require("../items/items-router.js");
+
+module.exports = server => {
+  server.use("/auth", authRouter);
+  server.use("/users", usersRouter);
+  server.use("/items", itemsRouter);
+};
