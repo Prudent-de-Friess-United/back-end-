763ea8806c4c6bc6f2acd06d9f4d6a126e5f332b
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-02-04 11:54:48 -0500

    added Testing for login/reg/ server, etc... Updated Documentations
    Co-authored-by: ikeman32<ikeman32@yahoo.com>

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,20 +3,80 @@ const request = require("supertest");
 const server = require("./server.js");
 const db = require("../data/dbConfig.js");
 
-// THESE BELOW PASS
-
 it("should set db ENV to testing", function() {
   expect(process.env.DB_ENV).toBe("testing");
 });
 
-describe("server", function() {
-  describe("GET /", function() {
-    it("should return 200 OK", function() {
-      return request(server)
-        .get("/")
-        .then(res => {
-          expect(res.status).toBe(200);
-        });
+describe("SERVER & AUTHORIZATION", () => {
+  describe("Server", function() {
+    describe("GET /", function() {
+      it("should return 200 OK", function() {
+        return request(server)
+          .get("/")
+          .then(res => {
+            expect(res.status).toBe(200);
+          });
+      });
+      // it("Needs JSON formatted response", async () => {
+      //   const res = await request(server).get("/");
+      //   expect(res.type).toMatch(/json/i);
+      // });
+    });
+  });
+
+  describe("POST /register", () => {
+    describe("adds user", () => {
+      beforeEach(async () => {
+        await db("users").truncate();
+      });
+
+      // it("needs to return 201 OK", async () => {
+      //   const res = await request(server)
+      //     .post("/api/auth/register")
+      //     .send({
+      //       username: "mud",
+      //       password: "password",
+      //       department: "buyer"
+      //     });
+      //   expect(res.status).toBe(201);
+      // });
+
+      it("validates", async () => {
+        const res = await request(server)
+          .post("/auth/register")
+          .send({
+            username: "mud",
+            password: "password",
+            department: "buyer"
+          });
+        expect(res.body.error).toBe(undefined);
+      });
+    });
+  });
+
+  describe("POST /login", () => {
+    describe("log in user", () => {
+      it("needs to return 200 OK", async () => {
+        const res = await request(server)
+          .post("/auth/login")
+          .send({
+            username: "mud",
+            password: "password",
+            department: "buyer"
+          });
+        expect(res.status).toBe(200);
+      });
+
+      it("returns json", async () => {
+        const res = await request(server)
+          .post("/auth/login")
+          .send({
+            username: "mud",
+            password: "password",
+            department: "buyer"
+          });
+        expect(res.type).toMatch(/json/i);
+      });
     });
   });
 });
